f008a6e5c7cdfd8a331c6437b853d6bb371ad3a4
Pablo Markaide <pablo.markaide@gmail.com>
2024-11-26 11:52:13 +0200

    feat: add the ex00 exercise finished

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,30 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:02:50 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/11/26 11:23:12 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/11/26 11:51:35 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream> 
+#include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+	if(argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		std::string str;
+
+		for (int i = 1; i < argc; i++)
+		{
+			str += argv[i];
+			if (i < argc - 1)
+				str += " ";
+		}
+		for (char& c : str)
+			c = std::toupper(c);
+
+		std::cout << str << std::endl;
+	}
+	return 0;
 }
