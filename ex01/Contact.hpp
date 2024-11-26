1eadce99a1b86599897d292587bca7f4302e3a84
Pablo Markaide <pablo.markaide@gmail.com>
2024-11-26 16:51:52 +0200

    wip: add files for the two classes

---
+++ ex01/Contact.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 16:43:12 by pmarkaid          #+#    #+#             */
+/*   Updated: 2024/11/26 16:50:24 by pmarkaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+ #define CONTACT_HPP
+
+#include <string>
+
+class ClassName {
+   public:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+
+    
+
+   private:
+       // Add member variables here
+};
+
+#endif  // CONTACT_HPP
