a34c83504fe2b417fa6d95f13e78159073c8bb47
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-05 16:13:23 +0200

    feat: add ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 15:46:44 by pmarkaid          #+#    #+#             */
+/*   Updated: 2024/12/05 16:13:05 by pmarkaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <string>
+# include <iostream>
+
+int main()
+{
+	std::string brain = "HI THIS IS BRAIN";
+	std::string* stringPTR = &brain;
+	std::string& stringREF = brain;
+
+	// Print the address
+	std::cout << "\nString: " << &brain << std::endl;
+	std::cout << "Pointer: " << &stringPTR << std::endl;
+	std::cout << "Reference: " << &stringREF << std::endl;
+
+	// Print the value
+	std::cout << "\nString: " << brain << std::endl; // Direct access
+	std::cout << "Pointer: " << *stringPTR << std::endl; // Dereferencing pointer
+	std::cout << "Reference: " << stringREF << std::endl; // Using reference
+
+	
+	
+	return 0;
+}
