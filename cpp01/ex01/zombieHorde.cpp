27298a0c07b668177cdce80d106e2fa2054f81f8
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-05 15:25:06 +0200

    fix: use same name for the horde zombies

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:34:38 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/12/05 15:16:24 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/12/05 15:24:47 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ Zombie* zombieHorde(int N, std::string name) {
 	Zombie* horde = new Zombie[N];
 
 	for (int i = 0; i < N; ++i) {
-		std::string zombieName = name + std::to_string(i + 1);
-		horde[i].setName(zombieName);
+		//std::string zombieName = name + std::to_string(i + 1);
+		//horde[i].setName(zombieName);
+		horde[i].setName(name);
 		horde[i].announce();
 	}
 
