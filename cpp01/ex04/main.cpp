5b31026dacfc9186174fc631b7f0a2750b06bb91
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-06 11:30:35 +0200

    feat: add skeleton for ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 11:21:23 by pmarkaid          #+#    #+#             */
+/*   Updated: 2024/12/06 11:28:46 by pmarkaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "replace.hpp"
+
+int main(int argc, char const *argv[])
+{
+	if(argc != 4)
+	{
+		std::cout << "Invalid number of arguments\n\n";
+		std::cout << "\tUsage: ./replace file old_string new_string\n\n";
+		std::cout << "OLD_STRING will be replaced with NEW_STRING at every ocurrence in FILE\n";
+		return 1;
+	}
+	replace(argv[1], argv[2], argv[3]);
+	return 0;
+}
