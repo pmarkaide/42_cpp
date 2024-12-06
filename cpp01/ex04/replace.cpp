afefddd4df67a01092684a1e59edda836b27ef17
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-06 12:22:53 +0200

    feat: add infile read function

--- cpp01/ex04/replace.cpp
+++ cpp01/ex04/replace.cpp
@@ -6,12 +6,36 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 11:26:54 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/12/06 11:29:50 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/12/06 12:22:36 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "replace.hpp"
+# include <fstream>
+# include <iostream>
 
-void replace(std::string file, std::string s1, std::string s2){
+std::string read_file(std::string infile){
+	std::string line;
+	std::string fileContents;
+	std::ifstream file(infile);
+	if (!file.is_open()) {
+		std::cerr << "Failed to open the file!" << std::endl;
+	}else {
+		while (std::getline(file, line)) {
+			fileContents += line + "\n";
+		}
+	}
+	file.close();
+	return fileContents;
+}
+
+
+void replace(std::string infile, std::string s1, std::string s2){
+	// check infile
+	// read infile into a string
+
+	// search s1 index
+	// substitute by s2
+	// write into outfile
 	
 }
--- cpp01/ex04/replace.hpp
+++ cpp01/ex04/replace.hpp
@@ -6,11 +6,11 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 11:27:08 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/12/06 11:29:40 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/12/06 11:30:57 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 # include <string>
 
-void replace(std::string file, std::string s1, std::string s2);
+void replace(std::string infile, std::string s1, std::string s2);
