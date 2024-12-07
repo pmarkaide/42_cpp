91f430e30d67b8545303881eacc99317ddee248f
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-07 18:28:26 +0200

    fix: correct case numbers; fix message text

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 18:00:38 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/12/07 18:19:17 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/12/07 18:27:34 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 # include "Harl.hpp"
 
 void Harl::debug( void ){
+	std::cout << "[DEBUG]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info( void ){
+	std::cout << "[INFO]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning( void ){
+	std::cout << "[WARNING]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error( void ){
+	std::cout << "[ERROR]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -38,27 +42,27 @@ void Harl::complain( std::string level ){
 		};
 
 	std::map<std::string, int>::iterator it = levelMap.find(level);
-	int levelCode;
+	int levelCode = -1;
 	if (it != levelMap.end())
 		levelCode = it->second;
 
 	switch (levelCode) {
-		case 1:
+		case 0:
 			debug();
 			info();
 			warning();
 			error();
 			break;
-		case 2:
+		case 1:
 			info();
 			warning();
 			error();
 			break;
-		case 3:
+		case 2:
 			warning();
 			error();
 			break;
-		case 4:
+		case 3:
 			error();
 			break;
 		default:
