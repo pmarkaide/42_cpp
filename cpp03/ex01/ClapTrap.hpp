b161558411e842717bae07b5048b876204972f45
Pablo Markaide <pablo.markaide@gmail.com>
2025-01-06 12:41:23 +0200

    feat: add cpp03/ex01 skeleton files based on ex00

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 10:22:16 by pmarkaid          #+#    #+#             */
+/*   Updated: 2025/01/06 11:34:33 by pmarkaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+ #define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap{
+	private:
+		std::string name_;
+		int hitPoints_;
+		int energyPoints_;
+		int attackDamage_;
+		
+	public:
+		ClapTrap(const std::string& name);  // Constructor
+		ClapTrap(const ClapTrap& src);  // Copy constructor
+		ClapTrap &operator=(const ClapTrap& src); // Copy Assignment operator
+		~ClapTrap();
+		
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif // CLAPTRAP_HPP
