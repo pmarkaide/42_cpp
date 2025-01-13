dc37587dfe2cb7fd8c936a88115ec2556f8219d2
Pablo Markaide <pablo.markaide@gmail.com>
2025-01-13 13:27:16 +0200

    fix: don't accept empty input in cpp00ex00; remove spaces

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:02:50 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/11/26 11:51:35 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/01/13 13:26:44 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,14 @@ int main(int argc, char **argv) {
 		std::string str;
 
 		for (int i = 1; i < argc; i++)
-		{
 			str += argv[i];
-			if (i < argc - 1)
-				str += " ";
-		}
 		for (char& c : str)
 			c = std::toupper(c);
-
-		std::cout << str << std::endl;
+		if(str.empty()){
+			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+			return 0;
+		}else
+			std::cout << str << std::endl;
 	}
 	return 0;
 }
