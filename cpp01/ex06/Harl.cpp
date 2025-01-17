b3dd3f77803fd8fa2c746086aaf9f0ac0cbf7291
Pablo Markaide <pablo.markaide@gmail.com>
2025-01-17 11:38:29 +0200

    fix: remove map from Harl

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 18:00:38 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/12/07 18:27:34 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/01/17 11:37:51 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,37 @@
 
 void Harl::debug( void ){
 	std::cout << "[DEBUG]" << std::endl;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI really do!\n" << std::endl;
 }
 
 void Harl::info( void ){
 	std::cout << "[INFO]" << std::endl;
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
 void Harl::warning( void ){
 	std::cout << "[WARNING]" << std::endl;
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 
 void Harl::error( void ){
 	std::cout << "[ERROR]" << std::endl;
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
 
 void Harl::complain( std::string level ){
-	std::map<std::string, int> levelMap = {
-			{"[DEBUG]", 0},
-			{"[INFO]", 1},
-			{"[WARNING]", 2},
-			{"[ERROR]", 3}
-		};
 
-	std::map<std::string, int>::iterator it = levelMap.find(level);
-	int levelCode = -1;
-	if (it != levelMap.end())
-		levelCode = it->second;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-	switch (levelCode) {
+	int size = 4;
+	int i = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (level == levels[i])
+			break;
+	}
+
+	switch (i) {
 		case 0:
 			debug();
 			info();
