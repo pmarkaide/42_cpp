0056cdcc409476243d090c3f1a46a804db8bb0b1
Pablo Markaide <pablo.markaide@gmail.com>
2025-04-15 17:35:36 +0200

    refactor: simplify Data

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -6,15 +6,15 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 17:16:22 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/15 17:17:14 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/15 17:23:16 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DATA_HPP
 #define DATA_HPP
 
-struct Data {
+typedef struct Data {
 	int value;
-};
+} Data;
 
 #endif // DATA_HPP
