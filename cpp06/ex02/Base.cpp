17f7ad25119b83deee9a0dc162c44e6d7d0507b9
Pablo Markaide <pablo.markaide@gmail.com>
2025-04-24 12:41:13 +0300

    feat: add instance generator

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -6,11 +6,30 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:34:13 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/23 16:34:35 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/24 12:40:46 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 Base::~Base() {
+}
+
+Base* generate(void) {
+		
+	int random = rand() % 3;
+		
+	switch (random) {
+		case 0:
+			return new A();
+		case 1:
+			return new B();
+		case 2:
+			return new C();
+		default:
+			return NULL;
+	}
 }
--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -6,16 +6,29 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:31:17 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/23 16:33:55 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/24 12:38:53 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 #define BASE_HPP
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 class Base {
 public:
     virtual ~Base();
 };
+
+// Randomly creates an A, B, or C object and returns it as a Base pointer
+Base * generate(void);
+
+// Identifies and prints the type of the object pointed to by p
+void identify(Base* p);
+
+// Identifies and prints the type of the object referenced by p
+void identify(Base& p);
 
 #endif // BASE_HPP
