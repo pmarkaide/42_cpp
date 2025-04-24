a730c34b070743d1256b1df39cc762c23a18a7ce
Pablo Markaide <pablo.markaide@gmail.com>
2025-04-24 21:29:14 +0300

    refactor: use template function as argument

--- cpp07/ex01/Iter.hpp
+++ cpp07/ex01/Iter.hpp
@@ -6,28 +6,24 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 21:00:55 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/24 21:03:44 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/24 21:28:00 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Iter_HPP
-# define Iter_HPP
+#ifndef ITER_HPP
+# define ITER_HPP
 
-# include <cstddef>
-# include <iostream>
+#include <cstddef> // size_t
 
+template<typename T, typename Func>
+void iter(T* array, size_t length, Func func) {
+	if (!array || !func)
+		return;
 
-template<typename T>
-void iter(T* array, size_t length, void (*func)(T&)) {
 	for (size_t i = 0; i < length; i++) {
 		func(array[i]);
 	}
 }
 
-
-void print(int& x) {
-	std::cout << x << " ";
-}
-
-#endif // Iter_HPP
+#endif // ITER_HPP
 
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 21:02:46 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/24 21:03:10 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/24 21:27:36 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,36 @@
 #include "Iter.hpp"
 
 
+void multiply(int & x) {
+	x *= 2;
+}
+
+template<typename T>
+void print(T const & x) {
+	std::cout << x << " ";
+}
+
 int main() {
-	int arr[] = {1, 2, 3, 4, 5};
-	iter(arr, 5, print);
+	int intArray[] = {1, 2, 3, 4, 5};
+	std::string strArray[] = {"Hello", "world", "!"};
+		
+	// Test with integers and print function
+	std::cout << "Int array: ";
+	iter(intArray, 5, print<int>);
+	std::cout << std::endl;
+		
+	// Test with strings and print function
+	std::cout << "String array: ";
+	iter(strArray, 3, print<std::string>);
+	std::cout << std::endl;
+		
+	// Test with normal function
+	iter(intArray, 5, multiply);
+		
+	// Print after modification
+	std::cout << "After multiplication: ";
+	iter(intArray, 5, print<int>);
+	std::cout << std::endl;
+		
 	return 0;
 }
