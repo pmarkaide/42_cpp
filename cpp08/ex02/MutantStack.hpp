4e8ae08d01f9aa022e7987ec87edd1cf3189e3e5
Pablo Markaide <pablo.markaide@gmail.com>
2025-04-30 10:45:28 +0300

    feat: add iterators based on deque

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 10:12:30 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/30 10:32:19 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/30 10:42:41 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
 template <typename T>
-class MutantStack : public std::stack<T> {
+class MutantStack : public std::stack<T>
+{
 public:
-	MutantStack() : std::stack<T()> {}
+	MutantStack() : std::stack<T>() {}
 	MutantStack(const MutantStack& other) : std::stack<T>(other) {}
 	MutantStack& operator=(const MutantStack& other) {
 		if (this != &other)
@@ -26,6 +28,17 @@ public:
 		return *this;
 	}
 	~MutantStack() {}
+
+	// Iterator typedefs
+	typedef typename std::deque<T>::iterator iterator;
+	typedef typename std::deque<T>::const_iterator const_iterator;
+
+	// Iterator methods
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+		
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
 };
 
 #endif // MUTANTSTACK_HPP
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,9 +6,52 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 10:12:36 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/04/30 10:13:15 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/04/30 10:44:27 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iostream>
 
+
+int main()
+{
+    // Create a MutantStack of integers
+    MutantStack<int> mstack;
+    
+    // Push some values
+    mstack.push(5);
+    mstack.push(17);
+    
+    // Display the top element
+    std::cout << "Top element: " << mstack.top() << std::endl;
+    
+    // Pop the top element
+    mstack.pop();
+    
+    // Display size
+    std::cout << "Size after pop: " << mstack.size() << std::endl;
+    
+    // Add more elements
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    
+    // Iterate through the stack using our new iterators
+    std::cout << "Stack contents using iterators:" << std::endl;
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    
+    // Create a standard stack from our MutantStack (should work due to inheritance)
+    std::stack<int> s(mstack);
+    std::cout << "Standard stack size: " << s.size() << std::endl;
+    
+    return 0;
+}
