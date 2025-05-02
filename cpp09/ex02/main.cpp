88f44f872ee6896c140b511c99dd69c146f95977
Pablo Markaide <pablo.markaide@gmail.com>
2025-05-02 14:53:44 +0300

    chore: improve main messaging and add sort tester

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 12:40:12 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/05/02 11:21:24 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/05/02 14:52:23 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 #include <iostream>
-#include <ctime>
+#include <chrono>
 
 int main(int argc, char **argv) {
     // Check for debug flag
@@ -49,8 +49,15 @@ int main(int argc, char **argv) {
     
     // Display unsorted sequence
     std::cout << "Before: ";
-    for (auto num : vec_sorter.vec_) {
-        std::cout << num << " ";
+    size_t displayLimit = 10;
+    size_t displayCount = vec_sorter.vec_.size() <= displayLimit ? vec_sorter.vec_.size() : displayLimit;
+    
+    for (size_t i = 0; i < displayCount; ++i) {
+        std::cout << vec_sorter.vec_[i] << " ";
+    }
+    
+    if (vec_sorter.vec_.size() > displayLimit) {
+        std::cout << "[...]";
     }
     std::cout << std::endl;
     
@@ -59,23 +66,41 @@ int main(int argc, char **argv) {
     std::deque<int> deq_copy = deq_sorter.vec_;
     
     // Sort with vector
-    clock_t vec_start = clock();
+    auto vec_start = std::chrono::high_resolution_clock::now();
     vec_sorter.sort(vec_copy);
-    clock_t vec_end = clock();
-    double vec_time = (double)(vec_end - vec_start) / CLOCKS_PER_SEC * 1000000;
+    auto vec_end = std::chrono::high_resolution_clock::now();
+    auto vec_time = std::chrono::duration_cast<std::chrono::microseconds>(vec_end - vec_start).count();
     
     // Sort with deque
-    clock_t deq_start = clock();
+    auto deq_start = std::chrono::high_resolution_clock::now();
     deq_sorter.sort(deq_copy);
-    clock_t deq_end = clock();
-    double deq_time = (double)(deq_end - deq_start) / CLOCKS_PER_SEC * 1000000;
+    auto deq_end = std::chrono::high_resolution_clock::now();
+    auto deq_time = std::chrono::duration_cast<std::chrono::microseconds>(deq_end - deq_start).count();
+    
+    // Test if sorted correctly
+    bool isSorted = true;
+    for (size_t i = 1; i < vec_copy.size(); ++i) {
+        if (vec_copy[i - 1] > vec_copy[i]) {
+            isSorted = false;
+            break;
+        }
+    }
     
     // Display results
-    std::cout << "After: ";
-    for (auto num : vec_copy) {
-        std::cout << num << " ";
+    std::cout << "After:  ";
+    displayCount = vec_copy.size() <= displayLimit ? vec_copy.size() : displayLimit;
+    
+    for (size_t i = 0; i < displayCount; ++i) {
+        std::cout << vec_copy[i] << " ";
+    }
+    
+    if (vec_copy.size() > displayLimit) {
+        std::cout << "[...]";
     }
     std::cout << std::endl;
+    
+    // Display sort validation result with color
+    std::cout << "Sort validation: " << (isSorted ? "\033[32mPASSED ✓\033[0m" : "\033[31mFAILED ✗\033[0m") << std::endl;
     
     std::cout << "Time to process a range of " << vec_copy.size() 
               << " elements with std::vector : " << vec_time << " us" << std::endl;
