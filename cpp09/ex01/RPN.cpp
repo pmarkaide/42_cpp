d0a1a3cb4d66edcbfb767477ebba65352982a353
Pablo Markaide <pablo.markaide@gmail.com>
2025-06-02 21:25:30 +0300

    chore: change names; add Makefile

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 12:11:39 by pmarkaid          #+#    #+#             */
-/*   Updated: 2025/06/01 19:08:24 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2025/06/02 21:05:04 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,40 @@
 #include <iostream>
 
 int RPN::solve(const std::string& expression) {
-    std::stack<int> stack;
+    std::stack<int> stack_;
     std::istringstream iss(expression);
     std::string token;
     
     while (iss >> token) {
         if (isOperator(token)) {
-            if (stack.size() < 2) {
+            if (stack_.size() < 2) {
                 std::cerr << "Error\n";
                 return 1;
             }
-            int b = stack.top(); stack.pop();
-            int a = stack.top(); stack.pop();
+            int b = stack_.top(); stack_.pop();
+            int a = stack_.top(); stack_.pop();
             
             if (token == "/" && b == 0) {
                 std::cerr << "Error\n";
                 return 1;
             }
             
-            stack.push(calculate(a, b, token[0]));
+            stack_.push(calculate(a, b, token[0]));
         } else {
             if (token.length() != 1 || !std::isdigit(token[0])) {
                 std::cerr << "Error\n";
                 return 1;
             }
-            stack.push(token[0] - '0');
+            stack_.push(token[0] - '0');
         }
     }
     
-    if (stack.size() != 1) {
+    if (stack_.size() != 1) {
         std::cerr << "Error\n";
         return 1;
     }
     
-    std::cout << stack.top() << std::endl;
+    std::cout << stack_.top() << std::endl;
     return 0;
 }
 
